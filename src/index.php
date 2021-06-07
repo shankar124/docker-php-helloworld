026ef35bc00dc5e244452f0ef41d5600f18a109a
shankar124 <shankarmishra124@gmail.com>
2021-06-07 20:32:59 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 
 echo "Hello, World from Docker! <br>";
-echo "Hello ECS!  version3 from ALB <br>";
+echo "Hello ECS!  version5 from ALB <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 
 ?>
